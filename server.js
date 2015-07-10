6d59d5c3b8c7604193d6a5fce77f76b94a11ee87
Peter Cardi <peter.v.cardi@gmail.com>
2015-07-10 16:08:55 -0600

    user can add new cars and view index

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var fs = require("fs"),
-    http = require("http"),
-    mongoose = require("mongoose"),
-    jade = require('jade');
+http = require("http"),
+mongoose = require("mongoose"),
+jade = require('jade');
 
 // load Car model
 var carAttrs = require("./car.js"),
-    carSchema = mongoose.Schema(carAttrs);
-
+carSchema = mongoose.Schema(carAttrs);
 
 var Car = mongoose.model('Car', carSchema);
 mongoose.connect('mongodb://localhost/crud_sans_frameworks');
@@ -16,28 +15,44 @@ var handleRequest = function(req, res) {
   if (req.url == '/') {
     res.writeHead(301, {Location: 'http://localhost:1337/cars'})
     res.end();
-  }
+  }//root path redirecting to cars
 
-  if (req.url == '/cars') {
-    // Synchronously load the index jade template (http://jade-lang.com/)
+  if (req.url === '/cars' && req.method === 'GET') {//INDEX GET
     var index = fs.readFileSync('index.jade', 'utf8');
-    // Compile template
     compiledIndex = jade.compile(index, { pretty: true, filename: 'index.jade' });
+    Car.find(function (err, cars) {
+      if (err) { return console.error(err); }
+      var rendered = compiledIndex({cars: cars});
+      res.end(rendered);
+    });
 
-    // example of data that can be passed in to the Jade template:
-    // in your CRUD app, a call to Mongoose should return all of the Cars
-    var sampleDataForCars = { cars: [
-      { driver: 'Andreas', make: 'Nissan', model: 'Xterra', year: 2005 },
-      { driver: 'Bob Ross', make: 'Ford', model: 'Pinto', year: 1972 }
-    ]};
+  } else if (req.url === '/cars' && req.method === 'POST') {//INDEX POST
+    req.on('data', function(data) {
+      var tempdata = data.toString().split('&');
+      tempdata.pop();
+      var temperdata = tempdata.map(function(param) {
+        return param.replace('car%5B', '');
+      });
+      var params = {};
+      temperdata.forEach(function(param) {
+        params[param.split('%5D=')[0]] = param.split('%5D=')[1].replace(/\+/g, ' ');
+      });
+      params.year = Number(params.year);
+      var car = new Car(params);
+      car.save(function(err) {
+        if (err) return console.error(err);
+      })
+      res.writeHead(301, {Location: 'http://localhost:1337/cars'})
+      res.end();
+    })
 
-    // Render jade template, passing in the info
-    var rendered = compiledIndex(sampleDataForCars);
-
-    // Write rendered contents to response stream
+  } else if (req.url === '/cars/new') {//NEW
+    var newCar = fs.readFileSync('new.jade', 'utf8');
+    compiledNew = jade.compile(newCar, { pretty: true, filename: 'new.jade' });
+    var rendered = compiledNew();
     res.end(rendered);
+
   } else {
-    // Your code might go here (or it might not)
     res.writeHead(200);
     res.end('A new programming journey awaits');
   }
